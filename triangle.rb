f2aed7812f5519a89aecc01b38fc8a19a3649117
Benjamin Ward <ward.programm3r@gmail.com>
2015-06-23 08:10:11 -0400

    Completed triangle project 2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+
   return :equilateral if a == b && a == c
   return :isosceles if (a == b && a != c) || (b == c && b != a) || (a == c && a != b)
   return :scalene
